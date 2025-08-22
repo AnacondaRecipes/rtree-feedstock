2ac967a0225f254f30b3bf7a8a20635cf2f10c55
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2025-08-22 11:56:01 +0200

    complete unification of Windows build

--- recipe/bld_.bat
+++
@@ -1,6 +0,0 @@
-set LIB=%LIBRARY_LIB%;%LIB%
-set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
-
-%PYTHON% -m pip install --no-deps --no-build-isolation --ignore-installed .
-if errorlevel 1 exit 1
